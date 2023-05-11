6b35fafb2f884bba252f07f928f96e00d9d16530
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-05-11 11:38:15 -0400

    started writing basic code to test h-bridge

--- code.py
+++ code.py
@@ -1,5 +1,13 @@
-from time import sleep
+import digitalio
+from analogio import AnalogIn, AnalogOut
+import board
 
-while True:
-    print("Hello world!")
-    sleep(1)
+L1=AnalogOut(board.D7)
+L2=AnalogOut(board.D6)
+
+R1=AnalogOut(board.D10)
+R2=AnalogOut(board.D11)
+
+while True: 
+    L1 = 30
+    L2 = 1
