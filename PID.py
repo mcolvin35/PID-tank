7f70094a1ad55e010baf2e5b7c57085027f42d0e
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-05-30 12:02:37 -0400

    started tuning pid

--- PID.py
+++ PID.py
@@ -2,9 +2,8 @@ from PID_CPY import PID
 import board
 from pwmio import PWMOut
 import adafruit_hcsr04
-import time 
 
-pid = PID(1000, 100, 50, setpoint=15, output_limits=(0, 65535))
+pid = PID(10, 0, 1, setpoint=15, output_limits=(-65535, 65535))
 dis=0
 
 
@@ -23,45 +22,25 @@ while True:
     try:
         dis = ((Lsens.distance+Rsens.distance)/2.0)
         control = pid(dis)
-        motor=int(control)
         p,i,d = pid.components
 
         print("DISTANCE:", dis,"\t", p,i,d, "\t MOTOR:", control) 
-        #print("\t", pid.proportional, pid.integral, pid.derivative)
         
-        # lcm=Lsens.distance
-        # rcm=Rsens.distance
+        lcm=Lsens.distance
+        rcm=Rsens.distance
 
-       # if rcm > 15 and lcm < 15:
-        L1.duty_cycle=motor
-        L2.duty_cycle=65000
+        if control < 0:
+            L1.duty_cycle=abs(int(control))
+            L2.duty_cycle=65535
 
-        R1.duty_cycle=motor
-        R2.duty_cycle=65000
+            R1.duty_cycle=abs(int(control))
+            R2.duty_cycle=65535
 
-        # if lcm > 15 and rcm < 15:
-        #     L1.duty_cycle=control
-        #     L2.duty_cycle=65000
+        if control > 0:
+            L1.duty_cycle=65535
+            L2.duty_cycle=abs(int(control))
 
-        #     R1.duty_cycle=65000
-        #     R2.duty_cycle=control
-        #     print("turn right")
-
-        # if lcm > 15 and rcm > 15: 
-        #     L1.duty_cycle=0
-        #     L2.duty_cycle=65000
-
-        #     R1.duty_cycle=0
-        #     R2.duty_cycle=65000
-        #     print("forward")
-
-        # if lcm < 15 and rcm < 15: 
-        #     L1.duty_cycle=65000
-        #     L2.duty_cycle=0
-
-        #     R1.duty_cycle=65000
-        #     R2.duty_cycle=0
-        #     print("backward")
+            R1.duty_cycle=65535
+            R2.duty_cycle=abs(int(control))
     except RuntimeError:
-        print("retrying!")
-        time.sleep(0.1)
+        print("Retrying!")
