7f5500e73e332522e3251bb5b6ca4a250eddbaf0
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-06-01 21:48:23 -0400

    commented code

--- PID.py
+++ PID.py
@@ -3,16 +3,17 @@ import board
 from pwmio import PWMOut
 import adafruit_hcsr04
 
-pid = PID(10, 0, 1, setpoint=15, output_limits=(-65535, 65535))
+pid = PID(10, 0, 1, setpoint=15, output_limits=(-65535, 65535)) #pid tuning
 dis=0
 
-
+#assigning pins to motors
 L1 = PWMOut(board.D2)
 L2 = PWMOut(board.D3)
 
 R1 = PWMOut(board.D8)
 R2 = PWMOut(board.D9)
 
+#assigning pins to sensors
 Lsens = adafruit_hcsr04.HCSR04(trigger_pin=board.A4, echo_pin=board.A5)
 Rsens = adafruit_hcsr04.HCSR04(trigger_pin=board.A0, echo_pin=board.A1)
 
@@ -20,27 +21,27 @@ Rsens = adafruit_hcsr04.HCSR04(trigger_pin=board.A0, echo_pin=board.A1)
 
 while True: 
     try:
-        dis = ((Lsens.distance+Rsens.distance)/2.0)
-        control = pid(dis)
+        dis = ((Lsens.distance+Rsens.distance)/2.0) #average distance between sensors
+        control = pid(dis) #pid calculation 
         p,i,d = pid.components
 
-        print("DISTANCE:", dis,"\t", p,i,d, "\t MOTOR:", control) 
+        print("DISTANCE:", dis,"\t", p,i,d, "\t MOTOR:", control) #distance, pid, and pid output
         
         lcm=Lsens.distance
         rcm=Rsens.distance
 
-        if control < 0:
-            L1.duty_cycle=abs(int(control))
+        if control < 0: #if pid output is less than 0
+            L1.duty_cycle=abs(int(control)) #left motor forwards absolute value of control (absolute value and int bc duty_cycle doesnt take negative numbers or decimals) 
             L2.duty_cycle=65535
 
-            R1.duty_cycle=abs(int(control))
+            R1.duty_cycle=abs(int(control)) #right motor forwards
             R2.duty_cycle=65535
 
-        if control > 0:
+        if control > 0: #if pid output is greater than 0
             L1.duty_cycle=65535
-            L2.duty_cycle=abs(int(control))
+            L2.duty_cycle=int(control)
 
             R1.duty_cycle=65535
-            R2.duty_cycle=abs(int(control))
+            R2.duty_cycle=int(control)
     except RuntimeError:
         print("Retrying!")
--- no_PID.py
+++ no_PID.py
@@ -4,12 +4,14 @@ from pwmio import PWMOut
 import adafruit_hcsr04
 import time 
 
+#assigning pins to motors
 L1 = PWMOut(board.D2)
 L2 = PWMOut(board.D3)
 
 R1 = PWMOut(board.D8)
 R2 = PWMOut(board.D9)
 
+#assigning pins to sensors
 Lsens = adafruit_hcsr04.HCSR04(trigger_pin=board.A4, echo_pin=board.A5)
 Rsens = adafruit_hcsr04.HCSR04(trigger_pin=board.A0, echo_pin=board.A1)
 
@@ -17,39 +19,40 @@ Rsens = adafruit_hcsr04.HCSR04(trigger_pin=board.A0, echo_pin=board.A1)
 
 while True: 
     try:
+        #creating variables to use sensor values
         lcm=Lsens.distance
         rcm=Rsens.distance
 
-        if rcm > 15 and lcm < 15:
-            L1.duty_cycle=65000
-            L2.duty_cycle=0
+        if rcm > 15 and lcm < 15: #if object is closer to left sensor
+            L1.duty_cycle=65000 
+            L2.duty_cycle=0 #left motor backwards
 
-            R1.duty_cycle=0
-            R2.duty_cycle=65000
+            R1.duty_cycle=0 #right motor forwards
+            R2.duty_cycle=65000 
             print("turn left")
 
-        if lcm > 15 and rcm < 15:
-            L1.duty_cycle=0
-            L2.duty_cycle=65000
+        if lcm > 15 and rcm < 15: #if object is closer to right sensor
+            L1.duty_cycle=0 #left motor forwards
+            L2.duty_cycle=65000 
 
             R1.duty_cycle=65000
-            R2.duty_cycle=0
+            R2.duty_cycle=0 #right motor backwards
             print("turn right")
 
-        if lcm > 15 and rcm > 15: 
-            L1.duty_cycle=0
+        if lcm > 15 and rcm > 15: #if object is farther than 15cm away
+            L1.duty_cycle=0 #left motor forwards
             L2.duty_cycle=65000
-
-            R1.duty_cycle=0
+            
+            R1.duty_cycle=0 #right motor forwards
             R2.duty_cycle=65000
             print("forward")
 
-        if lcm < 15 and rcm < 15: 
+        if lcm < 15 and rcm < 15: #if object is closer than 15cm
             L1.duty_cycle=65000
-            L2.duty_cycle=0
+            L2.duty_cycle=0 #left motor backwards
 
             R1.duty_cycle=65000
-            R2.duty_cycle=0
+            R2.duty_cycle=0 #right motor backwards
             print("backward")
     except RuntimeError:
         print("retrying!")
